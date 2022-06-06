cd37d3151c201a5f01e737d4d36f80544dbb4502
Logan Nguyen <nam.p.nguyen131@gmail.com>
2022-06-05 19:59:55 -0500

    Added images

--- app/src/main/java/dev/nguyen/affirmations/MainActivity.kt
+++ app/src/main/java/dev/nguyen/affirmations/MainActivity.kt
@@ -2,10 +2,23 @@ package dev.nguyen.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import dev.nguyen.affirmations.adapter.ItemAdapter
+import dev.nguyen.affirmations.data.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize data.
+        val myDataset = DataSource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
     }
 }

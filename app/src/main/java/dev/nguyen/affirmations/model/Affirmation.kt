6368bc70ab3ae8ab0b58ce4413e0b03c6cc685e6
Logan Nguyen <nam.p.nguyen131@gmail.com>
2022-06-05 20:09:16 -0500

    Update Affirmation.kt

--- app/src/main/java/dev/nguyen/affirmations/model/Affirmation.kt
+++ app/src/main/java/dev/nguyen/affirmations/model/Affirmation.kt
@@ -1,4 +1,9 @@
 package dev.nguyen.affirmations.model
 
-data class Affirmation(val stringResourceId: Int) {
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int) {
 }
